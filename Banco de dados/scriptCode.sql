3fc9606c8e70e684e6488ea4fffa93d472f7466e
IgorAnthonyy <igor.maciel@sptech.school>
2023-11-07 14:55:11 -0300

    Adicionando o scrip do banco de dados

---
+++ Banco de dados/scriptCode.sql
@@ -0,0 +1,34 @@
+create database codecraft;
+use codecraft;
+
+create table usuario (
+idUsuario int primary key auto_increment,
+email varchar(100),
+senha char(20),
+telefone char(11)
+);
+
+create table quizHtml (
+idTentativa int primary key auto_increment,
+qtdDeAcertos int,
+qtdDeErros int,
+fkUsuario int,
+constraint fkUhtml foreign key (fkUsuario)
+	references usuario(idUsuario)
+);
+create table quizCss (
+idTentativa int primary key auto_increment,
+qtdDeAcertos int,
+qtdDeErros int,
+fkUsuario int,
+constraint fkUcss foreign key (fkUsuario)
+	references usuario(idUsuario)
+);
+create table quizJs (
+idTentativa int primary key auto_increment,
+qtdDeAcertos int,
+qtdDeErros int,
+fkUsuario int,
+constraint fkUjs foreign key (fkUsuario)
+	references usuario(idUsuario)
+);
